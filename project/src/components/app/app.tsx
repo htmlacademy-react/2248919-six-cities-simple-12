8fa76af4daa0a2a1857df90c3b774c264ad24d9c
DmitryBadenkov <badenkov2003@gmail.com>
2023-04-25 05:33:35 +0300

    Отрисовал и создал компоненты для работы приложения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../../pages/main/main';
+
+type AppMain = {
+  cardCount: number;
+}
+
+function App({cardCount}:AppMain): JSX.Element {
+  return (
+    <Main cardCount = {cardCount} />
+  );
 }
 
 export default App;
